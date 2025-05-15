30eaa870c4989c67c4432662d0cc1194262dc905
Vishal Srinivasan <vishalvishal619@gmail.com>
2025-05-14 21:55:45 -0400

    Add JavaScript file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional JavaScript for adding interactivity
+// Example: Alert when the button is clicked
+
+$(document).ready(function() {
+    $(".btn-primary").click(function() {
+        alert("Button Clicked!");
+    });
+});
